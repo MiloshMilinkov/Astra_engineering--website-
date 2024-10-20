330af5d45c74042e64dd7b7b2964bef9831ed6c1
Milos Milinkov <milos300399@gmail.com>
2024-10-20 21:22:12 +0200

    Home View Scroll

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,10 @@
-<script setup lang="ts">
-  import Navbar from '../components/Navbar.vue';
-  import Gallery from '../components/Gallery.vue';
-  import Banner from '../components/Banner.vue';
-</script>
-
 <template>
-  <div >
+  <div>
     <main>
       <Banner />
-      <Gallery />
+      <div id="gallery">
+        <Gallery />
+      </div>
     </main>
   </div>
 </template>
@@ -16,3 +12,33 @@
 <style scoped>
 
 </style>
+
+<script setup lang="ts">
+  import { onMounted, onUnmounted } from 'vue';
+  import { useRouter } from 'vue-router';
+  import Gallery from '../components/Gallery.vue';
+  import Banner from '../components/Banner.vue';
+  const router = useRouter();
+
+  const handleScroll = () => {
+    const galleryElement = document.getElementById('gallery');
+    if (galleryElement) {
+      const rect = galleryElement.getBoundingClientRect();
+      const triggerTrashold = 300;
+      if (rect.top < window.innerHeight - triggerTrashold && rect.bottom >= triggerTrashold) {
+        router.push('/gallery');
+      }
+    }
+  };
+
+  onMounted(() => {
+    window.addEventListener('scroll', handleScroll);
+  });
+
+  onUnmounted(() => {
+    window.removeEventListener('scroll', handleScroll);
+  });
+</script>
+
+
+
