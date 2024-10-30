6627eed2a4ee9c3604ba12cfc605f9eb75f0396f
Milos Milinkov <milos300399@gmail.com>
2024-10-30 23:21:47 +0100

    Added a sitemap for texting using  vite-plugin-sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,15 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import  VitePluginSitemap  from 'vite-plugin-sitemap';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePluginSitemap({
+      hostname: 'https://astra-engineering.vercel.app', // Update with your actual site URL
+    })
   ],
   resolve: {
     alias: {
